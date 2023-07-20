8403b9a935bb5b1c3bec37db65f08cb88c6ca6a8
Venugopal Reddy Kallam <78490740+Venucoder@users.noreply.github.com>
2023-07-20 14:10:06 +0530

    Update and rename starter-code to starter-code.cpp

---
+++ starter-code.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define all(V) (V).begin(), (V).end()
+#define pb push_back
+typedef long long ll;
+typedef pair<int, int> pii;
+const int mod = (int)1e9+7;
+const int N = 1e6;
+
+void solve() {       
+}
+ 
+int main() {
+    #ifndef ONLINE_JUDGE    
+    freopen("input.txt", "r", stdin);    
+    freopen("output.txt", "w", stdout);
+    #endif
+ 
+    int t; cin >> t; while(t--)     
+    solve();
+}
